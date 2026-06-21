14fdb086c54e76818a6cc4417d8f431c358471af
ajjuu04 <ajtopper2004@gmail.com>
2026-06-22 02:53:24 +0530

    check

--- src/main/java/com/ajinkyajambhulkar/learing/RESTAPIs/controller/StudentController.java
+++ src/main/java/com/ajinkyajambhulkar/learing/RESTAPIs/controller/StudentController.java
@@ -1,4 +1,18 @@
 package com.ajinkyajambhulkar.learing.RESTAPIs.controller;
 
+
+import com.ajinkyajambhulkar.learing.RESTAPIs.dto.StudentDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+
+    @GetMapping("/student")
+    public StudentDto StudentDto(){
+        return new StudentDto(69L,"Ajinkya","aj@gmail.com");
+    }
+
+
+
 }
--- src/main/java/com/ajinkyajambhulkar/learing/RESTAPIs/dto/StudentDto.java
+++ src/main/java/com/ajinkyajambhulkar/learing/RESTAPIs/dto/StudentDto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class SrudentDto {
+public class StudentDto {
     private Long id;
     private String name;
     private String email;
